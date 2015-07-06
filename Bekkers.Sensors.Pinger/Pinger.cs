ec62905277829aa0871347aa3e2e16c3122e86d1
TheBekker <christianbekker@gmail.com>
2015-07-06 23:59:41 +0200

    rearrange stuff, trying out pinger

---
+++ Bekkers.Sensors.Pinger/Pinger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace Bekker.Sensors.Pinger
+{
+    public class Pinger
+    {
+        private readonly GpioPin _pin;
+        private readonly Stopwatch _pulseLength = new Stopwatch();
+        private long startOfPulseAt;
+        private long endOfPulse;
+        public int Distance;
+        public Pinger(int pin)
+        {
+            _pin = GpioController.GetDefault().OpenPin(pin);
+            _pin.SetDriveMode(GpioPinDriveMode.Output);
+            _pin.Write(GpioPinValue.Low);
+        }
+
+        public void Enable()
+        {
+            _pin.ValueChanged += _pin_ValueChanged;
+        }
+        public void Disable()
+        {
+            _pin.ValueChanged += null;
+        }
+
+        public void GetRange()
+        {
+            var mre = new ManualResetEvent(false);
+            _pin.Write(GpioPinValue.High);
+            mre.WaitOne(TimeSpan.FromMilliseconds(0.01));
+            _pin.Write(GpioPinValue.Low);
+        }
+
+        private void _pin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if(args.Edge == GpioPinEdge.RisingEdge)
+            {
+                Debug.WriteLine("startpulse");
+                startOfPulseAt = System.DateTime.Now.Ticks;
+            }
+
+            if (args.Edge == GpioPinEdge.FallingEdge)
+            {
+                Debug.WriteLine("endpulse");
+                endOfPulse = System.DateTime.Now.Ticks;
+
+                if(startOfPulseAt > 0)
+                {
+                    var ticks = (int)(endOfPulse - startOfPulseAt);
+                    Distance = ticks / 580;
+                    Debug.WriteLine(Distance);
+                }
+            }
+        }
+    }
+}
--- Example/MainPage.xaml.cs
+++ Example/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Bekker.Sensors.CMPS10;
+using Bekker.Sensors.Pinger;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,53 +28,62 @@ namespace Example
         public static CMPS10 Cmps;
         public MainPage()
         {
-            var tries = 0;
-            var maxTries = 3;
+            //var tries = 0;
+            //var maxTries = 3;
             this.InitializeComponent();
-            Unloaded += MainPage_Unloaded;
-            Cmps = new CMPS10();
-            Cmps.Init();
-            var timer = new Stopwatch();
-            timer.Start();
-            while (true)
-            {
-                if (Cmps._isInited)
-                {
-                    break;
-                }
-                else
-                {
-                    if (timer.ElapsedMilliseconds > 10000)
-                    {
-                        Debug.WriteLine("timeout...");
-                        tries++;
-                        if (tries > maxTries)
-                        {
-                            Debug.WriteLine("max tries...");
-                            throw new Exception("No connection to i2c");
-                        }
-                        else
-                        {
-                            Cmps.ReInit();
-                            Debug.WriteLine("reinit...");
-                            timer.Restart();
-                        }
-                        
-                    }
-                }
+            //Unloaded += MainPage_Unloaded;
+            //Cmps = new CMPS10();
+            //Cmps.Init();
+            //var timer = new Stopwatch();
+            //timer.Start();
+            //while (true)
+            //{
+            //    if (Cmps._isInited)
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        if (timer.ElapsedMilliseconds > 10000)
+            //        {
+            //            Debug.WriteLine("timeout...");
+            //            tries++;
+            //            if (tries > maxTries)
+            //            {
+            //                Debug.WriteLine("max tries...");
+            //                throw new Exception("No connection to i2c");
+            //            }
+            //            else
+            //            {
+            //                Cmps.ReInit();
+            //                Debug.WriteLine("reinit...");
+            //                timer.Restart();
+            //            }
 
-            }
+            //        }
+            //    }
+
+            //}
+            //var timer2 = new System.Threading.Timer((e) =>
+            //{
+            //    Debug.WriteLine(Cmps.GetBearing());
+            //}, null, 0, 1000);
+            //var test = "";
+            Debug.WriteLine("go");
+            var ping = new Pinger(5);
+            ping.Enable();
+
+
             var timer2 = new System.Threading.Timer((e) =>
             {
-                Debug.WriteLine(Cmps.GetBearing());
+                ping.GetRange();
             }, null, 0, 1000);
-            var test = "";
 
         }
 
         private void MainPage_Unloaded(object sender, RoutedEventArgs e)
         {
-            Cmps.Disconnect();
+            //Cmps.Disconnect();
         }
     }
 }
